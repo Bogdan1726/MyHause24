c28bbcce6a561cf41d83680f878ec2da9918aab8
Bogdan1726 <bogdan24ro@gmail.com>
2022-07-24 23:35:03 +0300

    edit conf docker

--- myhause24/myhause24/settings.py
+++ myhause24/myhause24/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['146.190.233.3', '0.0.0.0', '127.0.0.1']
 
 # Application definition
 
